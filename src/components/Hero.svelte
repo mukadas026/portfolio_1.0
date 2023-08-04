ffb6bfeb7e567144640cc1e4ebb4ce051f62876d
mukadas026 <mukadasm026@gmail.com>
2023-08-04 02:58:16 +0000

    add typewriter effect and make slideshow more responsive

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Navbar from "./Navbar.svelte"
 	import me1 from "../assets/me1.jpg"
 	import me4 from "../assets/me4.jpg"
@@ -7,43 +7,82 @@
 	const slide = [me1, me4, me3]
 
 	import Carousel from "svelte-carousel"
-    // import CustomDot from 'svelte-carousel'
+	// import CustomDot from 'svelte-carousel'
 	let carousel
+
+	let textArr: String[] = ["The name's Mukadas Maltiti", "a Web developer", "a YouTuber", "a Geek"]
+
+	let typer: String = ""
+
+	// setInterval(() => {
+
+	// }, 1000)
+	let i:number = 0
+	let j:number = 0
+	const typewriter = () => {
+		if(i < textArr.length){
+			if(j < textArr[i].length){
+				typer += textArr[i][j]
+				j++
+			}else{
+				if(typer !== ''){
+					typer = typer.slice(0, typer.length - 1)
+				}else{
+					i++
+					j = 0
+				}
+			}
+		}else{
+			i = 0
+			j = 0
+		}
+		if(typer.length < textArr[i]?.length){
+			setTimeout(typewriter, 100)
+		}else{
+			setTimeout(typewriter, 1000)
+		}
+	}
+	typewriter()
 </script>
 
 <header class="relative">
 	<Navbar />
 	<div class="absolute w-full top-0 left-0">
-		<div class="max-w-[1920px] w-full h-screen max-h-[1080px] overflow-hidden mx-auto">
-            <div>
-                <p>Hey there, 👋</p>
-            </div>
+		<div class="relative max-w-[1920px] w-full h-96 md:h-fit xl:h-screen md:max-h-[1080px] overflow-hidden mx-auto">
+			<div class="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-3 md:space-y-16">
+				<p class=" w-screen text-center text-white text-5xl md:text-[5rem] font-black md:font-bold">
+					Hey there, <span class="inline-block animate-wave origin-bottom">👋</span>
+				</p>
+				<p class="h-[1ch] w-screen text-center text-white text-3xl md:text-6xl lg:text-[5rem] font-bold md:font-medium" ><span class="border-r-2 md:border-r-4 border-red-500 pr-2">{typer}</span></p>
+			</div>
 			<Carousel
 				autoplay
 				infinite
-				autoplayDuration={2000}
-				pauseOnFocus
+				autoplayDuration={5000}
 				arrows={false}
 				bind:this={carousel}
-                let:currentPageIndex
-                let:pagesCount
-                let:showPage
+				let:currentPageIndex
+				let:pagesCount
+				let:showPage
 			>
 				{#each slide as img (img)}
 					<img
 						src={img}
 						alt="Mukadas Maltiti"
-						class="w-full h-full aspect-video object-center "
+						class="w-full h-96 md:h-auto aspect-auto object-cover object-center"
 					/>
 				{/each}
-                <div slot="dots" class="absolute bottom-4 w-20 mx-auto flex justify-around">
-                    <!-- {#each Array(slide.length) as _, pageIndex (pageIndex)}
+				<div
+					slot="dots"
+					class="absolute bottom-2 md:bottom-3 lg:bottom-4 w-20 mx-auto flex justify-around"
+				>
+					<!-- {#each Array(slide.length) as _, pageIndex (pageIndex)}
                         <div class={`w-4 h-4 rounded-full ${currentPageIndex === pageIndex ? 'bg-sec' : 'bg-gray-400'}`} on:click={() => showPage(pageIndex)}></div>
                     {/each} -->
-                        {#each Array(slide.length) as _, i (i)}
-                            <div class="w-3 h-3 rounded-full bg-sec"></div>
-                        {/each}
-                </div>
+					{#each Array(slide.length) as _, i (i)}
+						<div class="w-3 h-3 rounded-full bg-sec" />
+					{/each}
+				</div>
 			</Carousel>
 			<!-- <div class={`bg-[url('${me1}')]`}></div> -->
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      colors: {
-        'main': '#152238',
-        'sec': '#66D7EB'
-      },
-      backgroundImage: {
-        'img1': "url('/src/assets/me1.jpg')"
-      }
-    }
-  },
-  plugins: []
-};
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		extend: {
+			colors: {
+				main: "#152238",
+				sec: "#66D7EB",
+			},
+			backgroundImage: {
+				img1: "url('/src/assets/me1.jpg')",
+			},
+			animation: {
+				wave: "wave 2s linear infinite",
+			},
+			keyframes: {
+				wave: {
+					"50%": { transform: "rotate(10deg)" },
+					"100%": { transform: "rotate(0)" },
+				},
+			},
+		},
+	},
+	plugins: [],
+}
